d1b436045d5e20114a4fd84d8653222d3f0b7625
Lucas <lucas.fferreira1859@gmail.com>
2024-08-15 14:25:17 -0300

    terceiro commit: resolução do teste - incompleto

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -2,5 +2,9 @@ describe('Página Principal', () => {
   it('Deve renderizar h1 com o texto correto!', () => {
     cy.visit("http://localhost:3000")
     cy.get('[data-test="titulo-principal"]').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.get('[data-test="vantagem1"]').contains('Conta e cartão gratuitos')
+    cy.get('[data-test="vantagem2"]').contains('Saques sem custo')
+    cy.get('[data-test="vantagem3"]').contains('Programa de pontos')
+    cy.get('[data-test="vantagem4"]').contains('Seguro Dispositivos')
   })
 })
