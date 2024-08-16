fceb4b862aaad4a58308057d012d3f365ede247b
Lucas <lucas.fferreira1859@gmail.com>
2024-08-16 10:28:44 -0300

    :sparkles: feat: desafio finalizado.

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -4,23 +4,23 @@ describe('Página Principal', () => {
   })
   it('Deve renderizar h2 com o texto correto!', () => {
     cy.visit("http://localhost:3000")
-    cy.getByData('titulo-principal').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
-    cy.getByData('vantagens').contains('Vantagens do nosso banco:')
+    cy.verifyByData('titulo-principal', 'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+    cy.getByData('vantagens', 'Vantagens do nosso banco:')
   })
   it('Deve renderizar h3 com o texto correto!', () => {
     cy.visit("http://localhost:3000")
-    cy.getByData('titulo1').contains('Conta e cartão gratuitos') 
-    cy.getByData('titulo2').contains('Saques sem custo')
-    cy.getByData('titulo3').contains('Programa de pontos')
-    cy.getByData('titulo4').contains('Seguro Dispositivos')
+    cy.getByData('titulo1','Conta e cartão gratuitos') 
+    cy.getByData('titulo2','Saques sem custo')
+    cy.getByData('titulo3','Programa de pontos')
+    cy.getByData('titulo4','Seguro Dispositivos')
     
   })
   it('Deve renderizar p com o texto correto!', () => {
     cy.visit("http://localhost:3000")
-    cy.getByData('p1').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
-    cy.getByData('p2').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
-    cy.getByData('p3').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
-    cy.getByData('p4').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+    cy.getByData('p1','Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+    cy.getByData('p2','Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+    cy.getByData('p3','Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+    cy.getByData('p4','Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
   })
 })
 
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -26,3 +26,7 @@
 Cypress.Commands.add('getByData', (seletor) => {
     return cy.get(`[data-test=${seletor}]`)
 })
+
+Cypress.Commands.add('verifyByData', (seletor, text) => {
+    return cy.get(`[data-test=${seletor}]`).contains(text)
+})
