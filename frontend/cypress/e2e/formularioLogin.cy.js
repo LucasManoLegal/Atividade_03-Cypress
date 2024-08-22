c6bb9717328eb34b950f3afda5cdd950cb308485
Lucas <lucas.fferreira1859@gmail.com>
2024-08-22 09:54:28 -0300

    :sparkles: :feat página de teste'formularioCadastro criada + correção no teste formulário login'

--- frontend/cypress/e2e/formularioLogin.cy.js
+++ frontend/cypress/e2e/formularioLogin.cy.js
@@ -8,19 +8,32 @@ describe('Testando múltiplas páginas', () => {
   });
 
   it.only('Não deve permitir um campo em branco', () => {
+    cy.visit('/');
 
-    cy.visit('/')
+    cy.getByData('botao-login').click();
 
-    cy.getByData('botao-login').click()
-    
     // cy.getByData('email-input').type('') /* não passaremos nenhuma informação no campo email, comentando esta linha, pois o comando type não aceita parâmetro vazio */
-    
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório') /* a
-    
-    mensagem exibida deve ser: "o campo email é obrigatório" */
-    })
 
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório'); /* a
+    
+    mensagem exibida deve ser: "O campo email é obrigatório" */
+  });
+
+  it.only('Não deve permitir um campo em branco', () => {
+    cy.visit('/');
+
+    cy.getByData('botao-login').click();
+
+    cy.getByData('email-input').type('neilton@alura');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo de senha é obrigatório'); /* a
+    
+    mensagem exibida deve ser: "O campo de senha é obrigatório" */
+  });
 });
-
