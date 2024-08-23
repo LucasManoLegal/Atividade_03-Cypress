dfb74226986f6a10768ea0bb206367bc0140afd4
Lucas <lucas.fferreira1859@gmail.com>
2024-08-23 11:33:34 -0300

    :sparkles: feat: spec páginas atualizada(2).

--- frontend/cypress/e2e/paginas.cy.js
+++ frontend/cypress/e2e/paginas.cy.js
@@ -7,6 +7,7 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('botao-enviar').click();
     cy.getByData('app-home').find('a').eq(0).click()
     cy.getByData('titulo-transacao').contains('Nova Transação')
+    cy.location('pathname').should('eq','/home')
     })
   it('Deve conseguir acessar a página de cartões', () => {
   cy.visit('/')
@@ -14,8 +15,10 @@ describe('Testando múltiplas páginas', () => {
   cy.getByData('email-input').type('neilton@alura.com');
   cy.getByData('senha-input').type('123456');
   cy.getByData('botao-enviar').click();
+  cy.location('pathname').should('eq','/home')
   cy.getByData('app-home').find('a').eq(1).click()
   cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
+  cy.location('pathname').should('eq','/home/cartoes')
   })
   it('Deve conseguir acessar a página Serviços', () => {
     cy.visit('/')
@@ -23,8 +26,10 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('email-input').type('neilton@alura.com');
     cy.getByData('senha-input').type('123456');
     cy.getByData('botao-enviar').click();
+    cy.location('pathname').should('eq','/home')
     cy.getByData('app-home').find('a').eq(2).click()
     cy.getByData('servico').contains('Empréstimo')
+    cy.location('pathname').should('eq','/home/servicos')
     })
     it('Deve conseguir acessar a página Investimentos', () => {
       cy.visit('/')
@@ -32,7 +37,9 @@ describe('Testando múltiplas páginas', () => {
       cy.getByData('email-input').type('neilton@alura.com');
       cy.getByData('senha-input').type('123456');
       cy.getByData('botao-enviar').click();
+      cy.location('pathname').should('eq','/home')
       cy.getByData('app-home').find('a').eq(3).click()
       cy.getByData('titulo-investimentos').contains('Investimentos')
+      cy.location('pathname').should('eq','/home/investimentos')
       })
 })
